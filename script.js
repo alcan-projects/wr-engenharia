3d5225f5c95af34230938deb4d3ef2ee6ab153a8
leodeymison <leodeymisonbasquete@gmail.com>
2023-08-27 14:40:50 -0300

    move click and scroll suave

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const ScrollBar = (direction) => {
+    const element = document.getElementById("container_fluid");
 
+    if(direction === "next") {
+        element.scrollTop = element.scrollTop + element.clientHeight;
+    }
+    else if(direction === "back") {
+        element.scrollTop = element.scrollTop - element.clientHeight;
+    }
 }
